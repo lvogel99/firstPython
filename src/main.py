2d94fdbf557ad9600cc1c252751b0af76d835299
Leon <30776401+lvogel99@users.noreply.github.com>
2018-06-14 20:42:23 +0200

    github desktop test

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 print("this works")
+
+for i in range(1,11):
+    if (i%2 == 0):
+        print(i,"is even")
+    else:
+        print(i, "is odd")
+
+
+message  = '{}%'
+
+for i in range(1, 101):
+    print(message.format(i))
